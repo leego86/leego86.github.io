b811770f2b3e2bb13326ab309e4c1173f53fd9e8
leego <leego8697@gmail.com>
2025-06-05 21:52:42 +0800

    vault backup: 2025-06-05 21:52:42

--- public/sw.js
+++ public/sw.js
@@ -1,56 +1,51 @@
-const cacheName = 'todo-{{ now.Format "2006-01-02" }}';
-const staticAssets = [
-    './',
-    './index.html',
-    './manifest.json',
-    './docs/**/*',
-    './favicon/android-chrome-192x192.png',
-    './favicon/android-chrome-512x512.png',
-    './favicon/apple-touch-icon.png',
-    './favicon/favicon.ico',
-    './favicon/favicon-16x16.png',
-    './favicon/favicon-32x32.png',
-    './css/home.min.*.css',
-    './css/docs.min.*.css',
-    './js/home.min.*.js',
-    './js/docs.min.*.js',
+const cacheName = self.location.pathname
+const pages = [
+
 ];
 
-self.addEventListener('install', async e => {
-    const cache = await caches.open(cacheName);
-    await cache.addAll(staticAssets);
-    return self.skipWaiting();
+self.addEventListener("install", function (event) {
+  self.skipWaiting();
+
+  caches.open(cacheName).then((cache) => {
+    return cache.addAll(pages);
+  });
 });
 
-self.addEventListener('activate', e => {
-    self.clients.claim();
-});
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+  if (request.method !== "GET") {
+    return;
+  }
 
-self.addEventListener('fetch', async e => {
-    const req = e.request;
-    const url = new URL(req.url);
-
-    if (url.origin === location.origin) {
-        e.respondWith(cacheFirst(req));
+  /**
+   * @param {Response} response
+   * @returns {Promise<Response>}
+   */
+  function saveToCache(response) {
+    if (cacheable(response)) {
+      return caches
+        .open(cacheName)
+        .then((cache) => cache.put(request, response.clone()))
+        .then(() => response);
     } else {
-        e.respondWith(networkFirst(req));
+      return response;
     }
+  }
+
+  /**
+   * @param {Error} error
+   */
+  function serveFromCache(error) {
+    return caches.open(cacheName).then((cache) => cache.match(request.url));
+  }
+
+  /**
+   * @param {Response} response
+   * @returns {Boolean}
+   */
+  function cacheable(response) {
+    return response.type === "basic" && response.ok && !response.headers.has("Content-Disposition")
+  }
+
+  event.respondWith(fetch(request).then(saveToCache).catch(serveFromCache));
 });
-
-async function cacheFirst(req) {
-    const cache = await caches.open(cacheName);
-    const cached = await cache.match(req);
-    return cached || fetch(req);
-}
-
-async function networkFirst(req) {
-    const cache = await caches.open(cacheName);
-    try {
-        const fresh = await fetch(req);
-        cache.put(req, fresh.clone());
-        return fresh;
-    } catch (e) {
-        const cached = await cache.match(req);
-        return cached;
-    }
-}
